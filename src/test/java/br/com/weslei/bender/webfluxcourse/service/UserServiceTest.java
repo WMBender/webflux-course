1ee39c66c1a96569c12dd5f84bd040ee33b8f42a
weslei.bender <wesleimb@hotmail.com>
2024-06-13 12:27:36 -0300

    unit test to update, delete and handleNotFoundMono

--- src/test/java/br/com/weslei/bender/webfluxcourse/service/UserServiceTest.java
+++ src/test/java/br/com/weslei/bender/webfluxcourse/service/UserServiceTest.java
@@ -4,6 +4,7 @@ import br.com.weslei.bender.webfluxcourse.entity.User;
 import br.com.weslei.bender.webfluxcourse.mapper.UserMapper;
 import br.com.weslei.bender.webfluxcourse.model.request.UserRequest;
 import br.com.weslei.bender.webfluxcourse.repository.UserRepository;
+import br.com.weslei.bender.webfluxcourse.service.exception.ObjectNotFoundException;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentMatchers;
@@ -17,6 +18,7 @@ import reactor.test.StepVerifier;
 
 import java.util.Objects;
 
+import static java.lang.String.format;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.times;
@@ -79,5 +81,52 @@ class UserServiceTest {
                 .verify();
 
         verify(repository, times(1)).findAll();
+    }
+
+    @Test
+    void update() {
+        UserRequest request = new UserRequest("weslei", "wesleibender@mail.com", "123");
+        User entity = User.builder().build();
+
+        when(mapper.toEntity(any(UserRequest.class), any(User.class))).thenReturn(entity);
+        when(repository.findById(anyString())).thenReturn(Mono.just(entity));
+        when(repository.save(any(User.class))).thenReturn(Mono.just(entity));
+
+        Mono<User> result = service.update("123", request);
+
+        StepVerifier.create(result)
+                .expectNextMatches(user -> user.getClass() == User.class)
+                .expectComplete()
+                .verify();
+
+        verify(repository, times(1)).save(any(User.class));
+    }
+
+    @Test
+    void delete() {
+        User entity = User.builder().build();
+        when(repository.findAndRemove(anyString())).thenReturn(Mono.just(entity));
+
+        Mono<User> result = service.delete("123");
+
+        StepVerifier.create(result)
+                .expectNextMatches(user -> user.getClass() == User.class)
+                .expectComplete()
+                .verify();
+
+        verify(repository, times(1)).findAndRemove(anyString());
+    }
+
+    @Test
+    void handleNotFoundMono(){
+        when(repository.findById(anyString())).thenReturn(Mono.empty());
+        String id = "123";
+        try{
+            service.findById(id).block();
+        } catch (Exception ex){
+            assertEquals(ObjectNotFoundException.class, ex.getClass());
+            assertEquals(format("Object not found. Id: %s, Type: %s", id, User.class.getSimpleName()),
+                    ex.getMessage());
+        }
     }
 }
